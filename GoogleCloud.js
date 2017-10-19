76317b557b5b43ddd53a169c800d61ddf7abf5c3
zendil <zendil@users.noreply.github.com>
2017-10-19 15:02:11 -0400

    start on class GoogleCloud

---
+++ GoogleCloud.js
@@ -0,0 +1,24 @@
+const EventEmitter = require('events').EventEmitter;
+const btoa = require('btoa');
+const crypto = require('crypto');
+
+class GoogleCloud extends EventEmitter {
+	constructor() {
+		super();
+		
+		this.token = {
+			value : '',
+			expires : ''
+		};
+		
+		this.cache = {};
+		
+		this.forceUpdate = 24 * 60 * 60; //1 day
+	}
+	
+	checkToken() {
+		
+	}
+}
+
+module.exports = GoogleCloud;
