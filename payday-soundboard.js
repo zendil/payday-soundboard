a8f393207f3d2e5845921e4a854d29f03517b24b
zendil <zendil@users.noreply.github.com>
2017-10-21 18:18:01 -0400

    reformat imports/requires

--- payday-soundboard.js
+++ payday-soundboard.js
@@ -1,3 +1,10 @@
 const GoogleCloud = require('./GoogleCloud.js');
+const WebServer = require('./WebServer.js');
 
-GoogleCloud.isTokenGood();
+const GC = new GoogleCloud();
+const WS = new WebServer(GC);
+
+GC.refreshToken();
+GC.updateCache();
+
+WS.listen(8080);
