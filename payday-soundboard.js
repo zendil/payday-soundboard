d0e4204acb049805a4aed77d6b90e473a3a99f9f
zendil <zendil@users.noreply.github.com>
2017-10-23 17:04:52 -0400

    handle play events

--- payday-soundboard.js
+++ payday-soundboard.js
@@ -1,10 +1,23 @@
 const GoogleCloud = require('./GoogleCloud.js');
 const WebServer = require('./WebServer.js');
+const DiscordBot = require('./DiscordBot.js');
 
 const GC = new GoogleCloud();
 const WS = new WebServer(GC);
+const D = new DiscordBot();
+
+const operatingPort = process.env.PORT || 8080;
 
 GC.refreshToken();
 GC.updateCache();
 
-WS.listen(8080);
+WS.listen(operatingPort);
+
+WS.on('play', (mediapath) => {
+	GC.streamFile(mediapath).then((data) => {
+		D.playSound(data);
+	},
+	() => {
+		//Failed to fetch data
+	});
+});
